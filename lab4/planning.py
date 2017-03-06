b20fac3ae9cf8c2f75feefadc6535d1b3c802648
Nishant Roy <nroy@gatech.edu>
2017-03-06 16:11:11 -0500

    4pm

--- lab4/planning.py
+++ lab4/planning.py
@@ -8,6 +8,7 @@ import math
 import cozmo
 from cozmo.util import degrees, distance_mm
 from grid import *
+from enum import Enum
 
 
 def astar(grid, heuristic):
@@ -107,8 +108,20 @@ def cozmoBehavior(robot: cozmo.robot.Robot):
     """
 
     global grid, stopevent
+
+    robot.set_lift_height(0).wait_for_completed()
+    robot.set_head_angle(degrees(-5)).wait_for_completed()
+
+    goalFound = False
+    noObstacle = True
+
+    boxWidth = 25.4
+    boxHeight = 25.4
+    boxDiag = 25.4 * math.sqrt(2)
+
     cozmoX = 3
     cozmoY = 2
+    cozmoDirection = 0
 
     for i in range(0, 26):
         grid.addObstacle((i, 0))
@@ -118,26 +131,116 @@ def cozmoBehavior(robot: cozmo.robot.Robot):
         grid.addObstacle((0, j))
         grid.addObstacle((25, j))
 
-    grid.addGoal((13, 8))
+    path = []
+    pathIndex = 0
+
+    if robot.world.light_cubes[cozmo.objects.LightCube1Id].is_visible:
+        goalFound = True
+        cube1X = robot.world.light_cubes[cozmo.objects.LightCube1Id].pose.position.x
+        cube1Y = robot.world.light_cubes[cozmo.objects.LightCube1Id].pose.position.y
+        cube1X //= 25.4
+        cube1Y //= 25.4
+        cube1X += cozmoX
+        cube1Y += cozmoY
+        grid.addGoal((cube1X, cube1Y))
+        astar(grid, heuristic)
+        path = grid.getPath()
+        pathIndex = 0
+
+        cube2NotFound = True
+        cube3NotFound = True
+
 
     while not stopevent.is_set():
-        # lookaround = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
-        # print("------------")
-        # robot.set_lift_height(0).wait_for_completed()
-        # robot.set_head_angle(degrees(-5)).wait_for_completed()
 
-        # cubes = robot.world.wait_until_observe_num_objects(num=1, object_type=cozmo.objects.LightCube, timeout=1)
-        #
-        # if robot.world.light_cubes[cozmo.objects.LightCube1Id].is_visible:
-        #     xVal = robot.world.light_cubes[cozmo.objects.LightCube1Id].pose.position.x//25 + cozmoX
-        #     yVal = robot.world.light_cubes[cozmo.objects.LightCube1Id].pose.position.y//25 + cozmoY
-        #     grid.addGoal((xVal, yVal))
-        # else:
-        #     print("Going to center")
-        # grid.addGoal((13, 8))
-        grid.clearVisited()
-        astar(grid, heuristic)
-        print(grid.checkPath())
+        if robot.world.light_cubes[cozmo.objects.LightCube2Id].is_visible:
+            cube2NotFound = False
+
+            pass
+
+        if robot.world.light_cubes[cozmo.objects.LightCube3Id].is_visible:
+            cube3NotFound = False
+            pass
+
+        if not goalFound and robot.world.light_cubes[cozmo.objects.LightCube1Id].is_visible:
+            cube1X = robot.world.light_cubes[cozmo.objects.LightCube1Id].pose.position.x
+            cube1Y = robot.world.light_cubes[cozmo.objects.LightCube1Id].pose.position.y
+            if cozmoDirection % 90 == 0:
+                cube1X //= 25.4
+                cube1Y //= 25.4
+                cube1X += cozmoX
+                cube1Y += cozmoY
+                grid.addGoal((cube1X, cube1Y))
+                grid.clearStart()
+                grid.addStart((cozmoX, cozmoY))
+                astar(grid, heuristic)
+                path = grid.getPath()
+                pathIndex = 0
+            else:
+                if cube1Y < 0:
+                    robot.turn_in_place(degrees(45))
+                    cozmoDirection -= 45
+                    cozmoDirection %= 360
+                else:
+                    robot.turn_in_place(degrees(-45))
+                    cozmoDirection += 45
+                    cozmoDirection %= 360
+                continue
+
+
+def TurnToFace(current, nextDirection):
+    turnValue = nextDirection - current
+
+    if turnValue > 0:
+        if turnValue > 180:
+            turnValue = 360 - turnValue
+        else:
+            turnValue *= -1
+    elif turnValue < 0:
+        if turnValue < -180:
+            turnValue = -360 - turnValue
+        else:
+            turnValue *= -1
+
+    return turnValue
+
+
+def moveToBox(cozmoCoord, boxCoord, cozmoDirection):
+    leftmove = 0
+    rightmove = 0
+    boxX = boxCoord[0]
+    boxY = boxCoord[1]
+    cozmoX = cozmoCoord[0]
+    cozmoY = cozmoCoord[1]
+
+    if cozmoX == boxX:
+        if cozmoY < boxY:
+            # Go 25 in +y
+            pass
+        elif cozmoY > boxY:
+            # Go 25 in -y
+            pass
+    elif cozmoY == boxY:
+        if cozmoX < boxX:
+            # Go 25 in +x
+            pass
+        elif cozmoX > boxX:
+            # Go 25 in -x
+            pass
+    elif cozmoY < boxY:
+        if cozmoX < boxX:
+            # Go northwest
+            pass
+        elif cozmoX > boxX:
+            # Go southwest
+            pass
+    elif cozmoX < boxX:
+        if cozmoY > boxY:
+            # Go northeast
+            pass
+    elif cozmoX > boxX and cozmoY > boxY:
+        # Go southeast
+        pass
 
 
 ######################## DO NOT MODIFY CODE BELOW THIS LINE ####################################
@@ -151,8 +254,8 @@ class RobotThread(threading.Thread):
         threading.Thread.__init__(self, daemon=True)
 
     def run(self):
-        test()
-        # cozmo.run_program(cozmoBehavior)
+        # test()
+        cozmo.run_program(cozmoBehavior)
 
 
 # If run as executable, start RobotThread and launch visualizer with empty grid file
