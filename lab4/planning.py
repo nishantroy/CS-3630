3d646b2d950a11639afc6c8af66b42ed0ef62ec5
Nishant Roy <nroy@gatech.edu>
2017-03-06 02:28:15 -0500

    Detecting Cubes. Goal set to middle of grid, find path.
    Finding bad path.

--- lab4/planning.py
+++ lab4/planning.py
@@ -1,12 +1,13 @@
 # author1: Nishant Roy
 # author2: Madelyn Juby
 
-from grid import *
 from visualizer import *
 import threading
 from queue import PriorityQueue
 import math
 import cozmo
+from cozmo.util import degrees, distance_mm
+from grid import *
 
 
 def astar(grid, heuristic):
@@ -67,7 +68,7 @@ def heuristic(current, goal):
     a = int(goal[0])
     b = int(goal[1])
     euclDist = math.sqrt((x - a) ** 2 + (y - b) ** 2)
-    # manDist = (abs(x - a) + abs(y - b))
+    manDist = abs(x - a) + abs(y - b)
 
     return euclDist
 
@@ -85,9 +86,37 @@ def cozmoBehavior(robot: cozmo.robot.Robot):
     """
 
     global grid, stopevent
+    cozmoX = 3
+    cozmoY = 2
+
+    for i in range(0, 26):
+        grid.addObstacle((i, 0))
+        grid.addObstacle((i, 17))
+
+    for j in range(0, 18):
+        grid.addObstacle((0, j))
+        grid.addObstacle((25, j))
+
+    grid.addGoal((13, 8))
 
     while not stopevent.is_set():
-        pass  # Your code here
+        # lookaround = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+        # print("------------")
+        # robot.set_lift_height(0).wait_for_completed()
+        # robot.set_head_angle(degrees(-5)).wait_for_completed()
+
+        # cubes = robot.world.wait_until_observe_num_objects(num=1, object_type=cozmo.objects.LightCube, timeout=1)
+        #
+        # if robot.world.light_cubes[cozmo.objects.LightCube1Id].is_visible:
+        #     xVal = robot.world.light_cubes[cozmo.objects.LightCube1Id].pose.position.x//25 + cozmoX
+        #     yVal = robot.world.light_cubes[cozmo.objects.LightCube1Id].pose.position.y//25 + cozmoY
+        #     grid.addGoal((xVal, yVal))
+        # else:
+        #     print("Going to center")
+        # grid.addGoal((13, 8))
+
+        astar(grid, heuristic)
+        print(grid.checkPath())
 
 
 ######################## DO NOT MODIFY CODE BELOW THIS LINE ####################################
