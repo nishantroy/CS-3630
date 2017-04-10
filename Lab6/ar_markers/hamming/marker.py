e67eafcf1f59a049d88c34df34360a6eeff3123c
Nishant Roy <nroy@gatech.edu>
2017-04-09 20:32:33 -0400

    Turn to goal and go there

--- Lab6/ar_markers/hamming/marker.py
+++ Lab6/ar_markers/hamming/marker.py
@@ -21,7 +21,6 @@ class HammingMarker(object):
     def __repr__(self):
         return '<Marker id={} center={}>'.format(self.id, self.center)
 
-
     @property
     def center(self):
         if self.contours is None:
@@ -29,13 +28,11 @@ class HammingMarker(object):
         center_array = mean(self.contours, axis=0).flatten()
         return (int(center_array[0]), int(center_array[1]))
 
-
     @property
     def origin(self):
         if self.contours is None:
             return None
         return (int(self.contours[0][0][0]), int(self.contours[0][0][1]))
-
 
     def generate_image(self):
         img = zeros((MARKER_SIZE, MARKER_SIZE))
@@ -46,38 +43,35 @@ class HammingMarker(object):
                 val = 255
             img[coords[0], coords[1]] = int(val)
         # return img
-        output_img = zeros((MARKER_SIZE*ZOOM_RATIO, MARKER_SIZE*ZOOM_RATIO))
-        cv2.resize(img, dsize=((MARKER_SIZE*ZOOM_RATIO, MARKER_SIZE*ZOOM_RATIO)), dst=output_img, interpolation=cv2.INTER_NEAREST)
+        output_img = zeros((MARKER_SIZE * ZOOM_RATIO, MARKER_SIZE * ZOOM_RATIO))
+        cv2.resize(img, dsize=((MARKER_SIZE * ZOOM_RATIO, MARKER_SIZE * ZOOM_RATIO)), dst=output_img,
+                   interpolation=cv2.INTER_NEAREST)
         return output_img
-
 
     def draw_contour(self, img, color=(0, 255, 0), linewidth=2):
         if self.contours != None:
             cv2.drawContours(img, [self.contours], -1, color, linewidth)
 
-
     def draw_origin(self, img, color=(0, 0, 255), radius=2, linewidth=3):
         if self.origin != None:
             cv2.circle(img, self.origin, radius=radius, color=color, thickness=linewidth)
-
 
     def draw_local_frame(self, img, camK, linewidth=3):
         # only display when pose exist
         if self.size == None or self.rvec == None or self.tvec == None:
             return
         # get plot obj points
-        obj_points = np.array([(0,0,0), (0.8*self.size,0,0), (0,0.8*self.size,0), (0,0,0.8*self.size)], \
-            dtype='float32')
+        obj_points = np.array([(0, 0, 0), (0.8 * self.size, 0, 0), (0, 0.8 * self.size, 0), (0, 0, 0.8 * self.size)],
+                              dtype='float32')
         # convert to img points
-        img_points = cv2.projectPoints(obj_points, self.rvec, self.tvec, camK, np.array([0,0,0,0], \
-            dtype='float32'))[0]
-        cv2.line(img, tuple(img_points[0][0]), tuple(img_points[1][0]), (0,0,255),thickness=linewidth)
-        cv2.line(img, tuple(img_points[0][0]), tuple(img_points[2][0]), (0,255,0),thickness=linewidth)
-        cv2.line(img, tuple(img_points[0][0]), tuple(img_points[3][0]), (255,0,0),thickness=linewidth)
+        img_points = cv2.projectPoints(obj_points, self.rvec, self.tvec, camK, np.array([0, 0, 0, 0],
+                                                                                        dtype='float32'))[0]
+        cv2.line(img, tuple(img_points[0][0]), tuple(img_points[1][0]), (0, 0, 255), thickness=linewidth)
+        cv2.line(img, tuple(img_points[0][0]), tuple(img_points[2][0]), (0, 255, 0), thickness=linewidth)
+        cv2.line(img, tuple(img_points[0][0]), tuple(img_points[3][0]), (255, 0, 0), thickness=linewidth)
 
-
-    def highlite_marker(self, img, contour_color=(0, 255, 255), text_color=(255, 255, 0), \
-            linewidth=2, draw_frame=False, camK=None):
+    def highlite_marker(self, img, contour_color=(0, 255, 255), text_color=(255, 255, 0),
+                        linewidth=2, draw_frame=False, camK=None):
         self.draw_contour(img, color=contour_color, linewidth=linewidth)
         if draw_frame:
             if camK == None:
@@ -87,16 +81,13 @@ class HammingMarker(object):
             self.draw_origin(img)
         cv2.putText(img, str(self.id), self.center, cv2.FONT_HERSHEY_DUPLEX, 1, text_color)
 
-
     @classmethod
     def generate(cls):
         return HammingMarker(id=randint(4096))
 
-
     @property
     def id_as_binary(self):
         return binary_repr(self.id, width=12)
-
 
     @property
     def hamming_code(self):
--- Lab6/go_to_goal.py
+++ Lab6/go_to_goal.py
@@ -18,20 +18,20 @@ from particle import Particle, Robot
 from setting import *
 from particle_filter import *
 from utils import *
-
+from cozmo.util import *
 
 # camera params
 camK = np.matrix([[295, 0, 160], [0, 295, 120], [0, 0, 1]], dtype='float32')
 
-#marker size in inches
+# marker size in inches
 marker_size = 3.5
 
 # tmp cache
-last_pose = cozmo.util.Pose(0,0,0,angle_z=cozmo.util.Angle(degrees=0))
+last_pose = cozmo.util.Pose(0, 0, 0, angle_z=cozmo.util.Angle(degrees=0))
 flag_odom_init = False
 
 # goal location for the robot to drive to, (x, y, theta)
-goal = (6,10,0)
+goal = (6, 10, 0)
 
 # map
 Map_filename = "map_arena.json"
@@ -39,44 +39,42 @@ grid = CozGrid(Map_filename)
 gui = GUIWindow(grid)
 
 
-
 async def image_processing(robot):
-
     global camK, marker_size
 
     event = await robot.world.wait_for(cozmo.camera.EvtNewRawCameraImage, timeout=30)
 
     # convert camera image to opencv format
     opencv_image = np.asarray(event.image)
-    
+
     # detect markers
     markers = detect_markers(opencv_image, marker_size, camK)
-    
+
     # show markers
     for marker in markers:
         marker.highlite_marker(opencv_image, draw_frame=True, camK=camK)
-        #print("ID =", marker.id);
-        #print(marker.contours);
+        # print("ID =", marker.id);
+        # print(marker.contours);
     cv2.imshow("Markers", opencv_image)
 
     return markers
 
-#calculate marker pose
+
+# calculate marker pose
 def cvt_2Dmarker_measurements(ar_markers):
-    
-    marker2d_list = [];
-    
+    marker2d_list = []
+
     for m in ar_markers:
         R_1_2, J = cv2.Rodrigues(m.rvec)
-        R_1_1p = np.matrix([[0,0,1], [0,-1,0], [1,0,0]])
-        R_2_2p = np.matrix([[0,-1,0], [0,0,-1], [1,0,0]])
+        R_1_1p = np.matrix([[0, 0, 1], [0, -1, 0], [1, 0, 0]])
+        R_2_2p = np.matrix([[0, -1, 0], [0, 0, -1], [1, 0, 0]])
         R_2p_1p = np.matmul(np.matmul(inv(R_2_2p), inv(R_1_2)), R_1_1p)
-        #print('\n', R_2p_1p)
-        yaw = -math.atan2(R_2p_1p[2,0], R_2p_1p[0,0])
-        
+        # print('\n', R_2p_1p)
+        yaw = -math.atan2(R_2p_1p[2, 0], R_2p_1p[0, 0])
+
         x, y = m.tvec[2][0] + 0.5, -m.tvec[0][0]
-        print('x =', x, 'y =', y,'theta =', yaw)
-        
+        print('x =', x, 'y =', y, 'theta =', yaw)
+
         # remove any duplate markers
         dup_thresh = 2.0
         find_dup = False
@@ -85,34 +83,33 @@ def cvt_2Dmarker_measurements(ar_markers):
                 find_dup = True
                 break
         if not find_dup:
-            marker2d_list.append((x,y,math.degrees(yaw)))
+            marker2d_list.append((x, y, math.degrees(yaw)))
 
     return marker2d_list
 
 
-#compute robot odometry based on past and current pose
+# compute robot odometry based on past and current pose
 def compute_odometry(curr_pose, cvt_inch=True):
     global last_pose, flag_odom_init
     last_x, last_y, last_h = last_pose.position.x, last_pose.position.y, \
-        last_pose.rotation.angle_z.degrees
+                             last_pose.rotation.angle_z.degrees
     curr_x, curr_y, curr_h = curr_pose.position.x, curr_pose.position.y, \
-        curr_pose.rotation.angle_z.degrees
-    
-    dx, dy = rotate_point(curr_x-last_x, curr_y-last_y, -last_h)
+                             curr_pose.rotation.angle_z.degrees
+
+    dx, dy = rotate_point(curr_x - last_x, curr_y - last_y, -last_h)
     if cvt_inch:
         dx, dy = dx / 25.6, dy / 25.6
 
-    return (dx, dy, diff_heading_deg(curr_h, last_h))
+    return dx, dy, diff_heading_deg(curr_h, last_h)
 
-#particle filter functionality
+
+# particle filter functionality
 class ParticleFilter:
-
     def __init__(self, grid):
         self.particles = Particle.create_random(PARTICLE_COUNT, grid)
         self.grid = grid
 
     def update(self, odom, r_marker_list):
-
         # ---------- Motion model update ----------
         self.particles = motion_update(self.particles, odom)
 
@@ -122,29 +119,82 @@ class ParticleFilter:
         # ---------- Show current state ----------
         # Try to find current best estimate for display
         m_x, m_y, m_h, m_confident = compute_mean_pose(self.particles)
-        return (m_x, m_y, m_h, m_confident)
+        return m_x, m_y, m_h, m_confident
 
 
 async def run(robot: cozmo.robot.Robot):
-
     global flag_odom_init, last_pose
     global grid, gui
 
     # start streaming
     robot.camera.image_stream_enabled = True
 
-    #start particle filter
+    # start particle filter
     pf = ParticleFilter(grid)
 
-    ###################
+    while True:
+        currPose = robot.pose
+        poseDiff = compute_odometry(currPose)
 
-    ############YOUR CODE HERE#################
+        markers = await image_processing(robot)
+        markers = cvt_2Dmarker_measurements(markers)
 
-    ###################
+        meanEstimate = pf.update(poseDiff, markers)
+
+        gui.show_particles(pf.particles)
+        gui.show_mean(meanEstimate[0], meanEstimate[1], meanEstimate[2], meanEstimate[3])
+        gui.updated.set()
+
+        if meanEstimate[3]:
+            # PF thinks converged
+            # Go from where you think you are to goal
+            print("CONVERGED")
+            estimatedX = meanEstimate[0]
+            estimatedY = meanEstimate[1]
+            estimatedTh = meanEstimate[2]
+
+            face90 = -estimatedTh + 90
+            print(face90)
+            robot.stop_all_motors()
+            await robot.turn_in_place(degrees(face90), in_parallel=False, num_retries=0).wait_for_completed()
+
+
+            turnAngle = math.atan2(math.fabs(estimatedY - goal[1]), math.fabs(estimatedX - goal[0]))
+
+            currentToGoalX = goal[0] - estimatedX
+            currentToGoalY = goal[1] - estimatedY
+            turn = 0
+            faceGoal = 0
+
+            if currentToGoalX > 0 and currentToGoalY > 0:
+                turn = 0 - turnAngle
+                faceGoal = -((math.pi/2) - turnAngle)
+            elif currentToGoalX > 0 >= currentToGoalY:
+                turn = -(math.pi - turnAngle)
+                faceGoal = ((math.pi / 2) - turnAngle)
+            elif currentToGoalX <= 0 < currentToGoalY:
+                turn = -(turnAngle + math.pi / 2)
+                faceGoal = -(turnAngle + (math.pi/2))
+            elif currentToGoalX <= 0 and currentToGoalY <= 0:
+                turn = math.pi - turnAngle
+                faceGoal = turnAngle + math.pi/2
+
+            await robot.turn_in_place(cozmo.util.Angle(turn), in_parallel=False).wait_for_completed()
+            robot.stop_all_motors()
+
+            await robot.drive_straight(cozmo.util.Distance
+                                       (grid_distance(currentToGoalX, goal[0], currentToGoalY, goal[1])),
+                                       cozmo.util.Speed(25)).wait_for_completed()
+            await robot.turn_in_place(cozmo.util.Angle(faceGoal)).wait_for_completed()
+
+            await robot.say_text("Bitches I have arrive").wait_for_completed()
+
+        else:
+            # look around, turn in place and update filter
+            await robot.drive_wheels(10, -10, duration=0)
 
 
 class CozmoThread(threading.Thread):
-    
     def __init__(self):
         threading.Thread.__init__(self, daemon=False)
 
@@ -154,7 +204,6 @@ class CozmoThread(threading.Thread):
 
 
 if __name__ == '__main__':
-
     # cozmo thread
     cozmo_thread = CozmoThread()
     cozmo_thread.start()
@@ -163,4 +212,3 @@ if __name__ == '__main__':
     grid = CozGrid(Map_filename)
     gui = GUIWindow(grid)
     gui.start()
-
